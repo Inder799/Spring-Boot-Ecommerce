3f4c67ef8a6cc5ae8eb12c58b93ef28edf2baee0
Inder <kumarinder799@gmail.com>
2025-11-15 15:48:44 +0530

    Update product quantity in the cart

--- src/main/java/com/ecommerce/sb_ecom/controller/CartController.java
+++ src/main/java/com/ecommerce/sb_ecom/controller/CartController.java
@@ -46,4 +46,11 @@ public class CartController {
         CartDTO cartDTO = cartService.getCart(emailId, cartId);
         return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.OK);
     }
+
+    @PutMapping("/carts/products/{productId}/quantity/{operation}")
+    public ResponseEntity<CartDTO> updateCartProduct(@PathVariable Long productId,
+                                                     @PathVariable String operation) {
+        CartDTO cartDTO = cartService.updateProductQuantityInCart(productId, operation.equalsIgnoreCase("delete") ? -1 : 1);
+        return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.OK);
+    }
 }
