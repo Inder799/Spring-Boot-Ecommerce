02ab72d09ddae51362197419dd04cdacd2e515ef
Inder <kumarinder799@gmail.com>
2025-11-18 10:13:49 +0530

    Customising swagger documentation

--- src/main/java/com/ecommerce/sb_ecom/config/SwaggerConfig.java
+++ src/main/java/com/ecommerce/sb_ecom/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.ecommerce.sb_ecom.config;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +26,15 @@ public class SwaggerConfig {
                 .addList("Bearer Authentication");
 
         return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Boot eCommerce API")
+                        .version("1.0")
+                        .description("This is a Spring Boot Project for eCommerce")
+                        .license(new License().name("Apache 2.0").url("http://killcode.com"))
+                        .contact(new Contact().name("Inder Kumar").email("Inder@killcode.com").url("https://github.com/Inder799")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project Documentation")
+                        .url("http://killcode.com"))
                 .components(new Components()
                         .addSecuritySchemes("Bearer Authentication", bearerScheme))
                 .addSecurityItem(bearerRequirement);
